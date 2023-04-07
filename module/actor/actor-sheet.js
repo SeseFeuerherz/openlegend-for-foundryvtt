10f6bbd40c376a75328da55f03cff6b585ca4917
“Sebastian <sesemessner@gmail.com>
2023-04-07 09:58:19 +0200

    debug logs

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -35,8 +35,8 @@ export class OlActorSheet extends ActorSheet {
     const renderData = await super.getData(options);
     console.log("Open Legend | super.getData()");
     console.log(renderData);
-    console.log("Open Legend | this.actor");
-    console.log(this.actor);
+    console.log("Open Legend | this");
+    console.log(this);
 
     if (renderData.actions == undefined) {
       renderData.actions = [];
@@ -109,7 +109,38 @@ export class OlActorSheet extends ActorSheet {
     });
 
     // Move items up in their corresponding rows
-    html.find('.action-move-up').click(move_action_up.bind(this));
+    html.find('.action-move-up').click(ev => {
+      console.log("Open Legend | Move action up in character sheet");
+      console.log(ev);
+      console.log(this);
+      // Get the item to move up
+      const tag = ev.currentTarget;
+      const item = this.actor.items.get(tag.dataset.item); // TODO gets wrong actor when sheet is opened via token
+      console.log("Open Legend | Debug item " + tag.dataset.item);
+      console.log(item);
+      // Get this items current and new indexes
+      const curr_index = item.system.action.index;
+      const new_index = curr_index - 1;
+      console.log("Open Legend | Debug curr_index: " + curr_index + ", new_index: " +new_index);
+      // Skip if already at top
+      if (curr_index > 0) {
+        // Find the item above it
+        this.actor.items.forEach(_sub_item => {
+          if (_sub_item.system.action) {
+            const i = _sub_item.system.action.index;
+            if (i == new_index) {
+              // Get the actual owned item and update its index
+              const sub_item = this.actor.items.get(_sub_item._id);
+              console.log("Open Legend | Debug sub-item");
+              console.log(sub_item);
+              sub_item.update({"system.action.index": curr_index});
+            }
+          }
+        });
+        // Update the main items index
+        item.update({"system.action.index": new_index});
+      }
+    });
     html.find('.gear-move-up').click(move_gear_up.bind(this));
     html.find('.feat-move-up').click(move_feat_up.bind(this));
 
