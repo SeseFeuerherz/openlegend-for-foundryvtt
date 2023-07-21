4c64645474d3f0d6ebaf5033acb69f401bed982f
“Sebastian <sesemessner@gmail.com>
2023-07-21 16:19:34 +0200

    debug

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -298,6 +298,7 @@ export class OlActorSheet extends ActorSheet {
 
     // Roll using the appropriate logic -- item vs attribute
     if (dataset.item)
+      console.log(this);
       if (dataset.targettype == 'single')
         rollItemSingleTarget(this,actor, this.actor.items.get(dataset.item).data);
       else if (dataset.targettype == 'multi')
